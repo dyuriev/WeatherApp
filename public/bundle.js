cb1a9fdfd682a19a3d72d45a16295d4dd55476b3
Dmitriy I. Yuriev <Dmitrii_Iurev@epam.com>
2016-10-04 12:38:08 +0300

    Added About

--- public/bundle.js
+++ public/bundle.js
@@ -58,6 +58,7 @@
 
 	var Main = __webpack_require__(216);
 	var Weather = __webpack_require__(218);
+	var About = __webpack_require__(219);
 
 	ReactDOM.render(React.createElement(
 	  Router,
@@ -65,6 +66,7 @@
 	  React.createElement(
 	    Route,
 	    { path: '/', component: Main },
+	    React.createElement(Route, { path: 'about', component: About }),
 	    React.createElement(IndexRoute, { component: Weather })
 	  )
 	), document.getElementById('app'));
@@ -24880,14 +24882,33 @@
 
 	var React = __webpack_require__(1);
 
+	var _require = __webpack_require__(159);
+
+	var Link = _require.Link;
+
+
 	var Nav = React.createClass({
 	  displayName: 'Nav',
 
 	  render: function render() {
 	    return React.createElement(
-	      'h2',
+	      'div',
 	      null,
-	      'Nav Component'
+	      React.createElement(
+	        'h2',
+	        null,
+	        'Nav Component'
+	      ),
+	      React.createElement(
+	        Link,
+	        { to: '/' },
+	        'Get Weather'
+	      ),
+	      React.createElement(
+	        Link,
+	        { to: '/about' },
+	        'About'
+	      )
 	    );
 	  }
 	});
@@ -24915,6 +24936,28 @@
 	});
 
 	module.exports = Weather;
+
+/***/ },
+/* 219 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var React = __webpack_require__(1);
+
+	var About = React.createClass({
+	  displayName: 'About',
+
+	  render: function render() {
+	    return React.createElement(
+	      'h3',
+	      null,
+	      'About Component'
+	    );
+	  }
+	});
+
+	module.exports = About;
 
 /***/ }
 /******/ ]);
